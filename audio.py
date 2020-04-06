32df5c91eea35ad04cd9d7b4f79c35f812052e6d
Joseph O. Eichenhofer <joseph.eichenhofer@gmail.com>
2020-04-06 11:45:51 -0400

    complete audio generation with QPSK signal generation

--- audio.py
+++ audio.py
@@ -1,43 +1,66 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 
-def gen_waveform(qam_symbols: np.ndarray, sym_baud: int = 16, sample_hz: int = 8e3, carrier_hz: int = 1e3):
+def gen_waveform(qam_symbols: np.ndarray, filename: str, sym_baud: int = 32, sample_hz: int = 8e3, carrier_hz: int = 1e3):
     """
     Create a sampled audio waveform based on the provided QAM symbols.
 
-    The Skype encoding algorithm potentially uses a sample rate of 16kHz, so we will default to a sample rate that is
+    The SILK encoding algorithm potentially uses a sample rate of 16kHz, so we will default to a sample rate that is
     half of that in order to avoid major problems in the transcoding and compression pipeline. Similarly, our carrier
     frequency will be one-eigth of our own sample rate, allowing for no chance of missing the nyquist rate for sampling
     arbitrary modulations to the waveform.
 
     :param qam_symbols: 1d array of complex floats representing qam symbols to generate the audio
+    :param filename: save waveform (.wav and .plt) files to this name in the data directory
     :param sym_baud: frequency of symbols per second (NOT bits per second)
     :param sample_hz: sampling frequency to use for the generated waveform
     :param carrier_hz: carrier tone (cosine wave) frequency to modulate
 
     :return: sampled audio as 1d array of floats representing raw values at each sample
     """
+    # compute angular frequency and define time series of each symbol
     f_w = 2*np.pi*carrier_hz
     t_series = np.arange(0, 1/sym_baud, 1/sample_hz)
 
-    wave_samples = []
+    # precompute in-phase carrier and quadrature carrier
+    i_carrier = np.cos(f_w*t_series)
+    q_carrier = np.sin(f_w*t_series)
 
+    # start with raw carrier wave
+    wave_samples = i_carrier
+
+    # append each modulated wave per symbol
     for curr_qam_s in qam_symbols:
-        curr_samples = np.abs(curr_qam_s) * np.cos(f_w * t_series + np.angle(curr_qam_s))
+        # compute in-phase waveform
+        i_phase_wave = np.real(curr_qam_s) * i_carrier
+        # compute quadrature waveform
+        q_phase_wave = np.imag(curr_qam_s) * q_carrier
+        # generate wave
+        curr_samples = i_phase_wave - q_phase_wave
+        # append to existing wave
         wave_samples = np.append(wave_samples, curr_samples)
 
-    from datetime import datetime as dt
-    timestamp = dt.utcnow().isoformat().replace(':', '_')
-
-    # import matplotlib
-    # matplotlib.use('Qt5Agg')
-    # from matplotlib import pyplot as plt
-    # plt.plot(np.arange(0, len(wave_samples)/sample_hz, 1/sample_hz), wave_samples)
-    # plt.savefig('data/{}.png'.format(timestamp))
+    # plot the signal
+    plt.plot(np.arange(0, len(wave_samples)/sample_hz, 1/sample_hz), wave_samples)
+    plt.savefig('data/{}.png'.format(filename))
 
     import wavio
-    normalized = (wave_samples - np.min(wave_samples)) / np.ptp(wave_samples)
-    centered = 2*normalized - 1
-    scaled = np.int16(centered * 32767)  # max value of int16 = 32767
-    wavio.write('data/{}.wav'.format(timestamp), scaled, sample_hz, sampwidth=2)
+    normalized_wave = (wave_samples - np.min(wave_samples)) / np.ptp(wave_samples)
+    centered_wave = 2*normalized_wave - 1
+    scaled_wave = np.int16(centered_wave * 32767)  # max value of int16 = 32767
+    wavio.write('data/{}.wav'.format(filename), scaled_wave, sample_hz, sampwidth=2)
 
+
+def parse_waveform(filename: str, sym_baud: int = 32, sample_hz: int = 8e3, carrier_hz: int = 1e3) -> np.ndarray:
+    """
+    Read a sampled waveform and extract the symbols modulated into it.
+
+    :param filename: wave file to read (without the .wav extension)
+    :param sym_baud: frequency of symbols per second (NOT bits per second)
+    :param sample_hz: sampling frequency to use for the generated waveform
+    :param carrier_hz: carrier tone (cosine wave) frequency to modulate
+    :return: symbols extracted from the waveform
+    """
+
+    raise NotImplementedError()
--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import numpy as np
 
-from CodecModem import CodecModem
+from datetime import datetime as dt
+timestamp = dt.utcnow().isoformat().replace(':', '_')
 
-cm = CodecModem(2)
-data = np.random.randint(0, 2, (32,))
 
-modulated = cm.encode_modulate(data)
-demodulated = cm.demodulate_decode(modulated)
+if __name__ == '__main__':
+    from CodecModem import CodecModem
+    cm = CodecModem(2)
+    data = np.random.randint(0, 2, (32,))
 
-assert np.array_equal(data, demodulated)
+    # test encoding/modulation
+    modulated = cm.encode_modulate(data)
+    demodulated = cm.demodulate_decode(modulated)
+    assert np.array_equal(data, demodulated)
 
-from audio import gen_waveform
+    # test waveform generation and parsing
+    from audio import gen_waveform, parse_waveform
+    gen_waveform(modulated, timestamp, sym_baud=64)
+    parsed = parse_waveform(timestamp, sym_baud=64)
+    assert np.array_equal(modulated, parsed)
 
-gen_waveform(modulated, sym_baud=4)
+    # test full pipeline
+    data = np.random.randint(0, 2, (32,))
+    modulated = cm.demodulate_decode(data)
+    gen_waveform(modulated, '{}_full'.format(timestamp), sym_baud=64)
+    parsed = parse_waveform('{}_full'.format(timestamp), sym_baud=64)
+    demodulated = cm.demodulate_decode(parsed)
+    assert np.array_equal(data, demodulated)
+
