88a105a74a4187e6d0eb868617b7402bbf32b393
Joseph O. Eichenhofer <joseph.eichenhofer@gmail.com>
2020-04-07 01:03:13 -0400

    attempt symbol extraction from waveform

--- audio.py
+++ audio.py
@@ -20,12 +20,12 @@ def gen_waveform(qam_symbols: np.ndarray, filename: str, sym_baud: int = 32, sam
     :return: sampled audio as 1d array of floats representing raw values at each sample
     """
     # compute angular frequency and define time series of each symbol
-    f_w = 2*np.pi*carrier_hz
+    w_c = 2*np.pi*carrier_hz
     t_series = np.arange(0, 1/sym_baud, 1/sample_hz)
 
     # precompute in-phase carrier and quadrature carrier
-    i_carrier = np.cos(f_w*t_series)
-    q_carrier = np.sin(f_w*t_series)
+    i_carrier = np.cos(w_c*t_series)
+    q_carrier = np.sin(w_c*t_series)
 
     # start with raw carrier wave
     wave_samples = i_carrier
@@ -41,13 +41,10 @@ def gen_waveform(qam_symbols: np.ndarray, filename: str, sym_baud: int = 32, sam
         # append to existing wave
         wave_samples = np.append(wave_samples, curr_samples)
 
-    # plot the signal
-    plt.plot(np.arange(0, len(wave_samples)/sample_hz, 1/sample_hz), wave_samples)
-    plt.savefig('data/{}.png'.format(filename))
-
-    import wavio
     normalized_wave = (wave_samples - np.min(wave_samples)) / np.ptp(wave_samples)
     centered_wave = 2*normalized_wave - 1
+
+    import wavio
     scaled_wave = np.int16(centered_wave * 32767)  # max value of int16 = 32767
     wavio.write('data/{}.wav'.format(filename), scaled_wave, sample_hz, sampwidth=2)
 
@@ -62,5 +59,35 @@ def parse_waveform(filename: str, sym_baud: int = 32, sample_hz: int = 8e3, carr
     :param carrier_hz: carrier tone (cosine wave) frequency to modulate
     :return: symbols extracted from the waveform
     """
+    # compute angular frequency of carrier
+    w_c = 2*np.pi*carrier_hz
 
-    raise NotImplementedError()
+    import wavio
+    input_wave: np.ndarray = wavio.read('data/{}.wav'.format(filename)).data[:, 0]
+    scaled_wave = input_wave / np.maximum(np.abs(np.min(input_wave)), np.abs(np.max(input_wave)))
+    scaled_wave = scaled_wave * np.sqrt(2)  # TODO: get scale factor automatically, not assuming sqrt(1^2+1^2)
+
+    # average phasor across each symbol period
+    symbol_period = int(sample_hz / sym_baud)
+    curr_period_phasors = []
+
+    # collect one symbol per symbol period
+    parsed_symbols = np.empty((0,), dtype=complex)
+
+    for t, sample in enumerate(scaled_wave):
+        # compute angle at this sample
+        w_t = t * w_c / sample_hz
+        # calculate the in-phase component
+        i_phase = sample * np.cos(w_t)
+        # calculate the quadrature component
+        q_phase = -1 * sample * np.sin(w_t)
+
+        # calculate the recovered symbol
+        recovered = np.complex(i_phase, q_phase)
+        curr_period_phasors.append(recovered)
+
+        if t % symbol_period == symbol_period - 1:
+            parsed_symbols = np.append(parsed_symbols, np.mean(curr_period_phasors))
+            curr_period_phasors = []
+
+    return parsed_symbols[1:]  # ignore the training signal for now
