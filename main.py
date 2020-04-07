5687c773aa8a791f4f1726388bdfb66959b8eb88
Joseph O. Eichenhofer <joseph.eichenhofer@gmail.com>
2020-04-07 01:02:51 -0400

    simplify basic tests

--- main.py
+++ main.py
@@ -10,21 +10,16 @@ if __name__ == '__main__':
     data = np.random.randint(0, 2, (32,))
 
     # test encoding/modulation
-    modulated = cm.encode_modulate(data)
-    demodulated = cm.demodulate_decode(modulated)
-    assert np.array_equal(data, demodulated)
+    encoded = cm.encode_modulate(data)
+    decoded = cm.demodulate_decode(encoded)
+    assert np.array_equal(data, decoded)
 
     # test waveform generation and parsing
     from audio import gen_waveform, parse_waveform
-    gen_waveform(modulated, timestamp, sym_baud=64)
-    parsed = parse_waveform(timestamp, sym_baud=64)
-    assert np.array_equal(modulated, parsed)
 
     # test full pipeline
-    data = np.random.randint(0, 2, (32,))
-    modulated = cm.demodulate_decode(data)
-    gen_waveform(modulated, '{}_full'.format(timestamp), sym_baud=64)
-    parsed = parse_waveform('{}_full'.format(timestamp), sym_baud=64)
-    demodulated = cm.demodulate_decode(parsed)
-    assert np.array_equal(data, demodulated)
-
+    encoded = cm.encode_modulate(data)
+    gen_waveform(encoded, '{}_full'.format(timestamp))
+    parsed = parse_waveform('{}_full'.format(timestamp))
+    decoded = cm.demodulate_decode(parsed)
+    assert np.array_equal(data, decoded)
