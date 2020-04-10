d2e86cebe5d90cd6c3bddc9aaf3af6f1109b5cfe
Joseph O. Eichenhofer <joseph.eichenhofer@gmail.com>
2020-04-10 00:15:12 -0400

    test for prefix (need to implement stop bits later)

--- main.py
+++ main.py
@@ -7,19 +7,21 @@ timestamp = dt.utcnow().isoformat().replace(':', '_')
 if __name__ == '__main__':
     from CodecModem import CodecModem
     cm = CodecModem(2)
-    data = np.random.randint(0, 2, (32,))
+    data_tx = np.random.randint(0, 2, (2*8192,))
 
     # test encoding/modulation
-    encoded = cm.encode_modulate(data)
-    decoded = cm.demodulate_decode(encoded)
-    assert np.array_equal(data, decoded)
+    encoded = cm.encode_modulate(data_tx)
+    data_rx = cm.demodulate_decode(encoded)
+    assert np.array_equal(data_tx, data_rx)
 
     # test waveform generation and parsing
     from audio import gen_waveform, parse_waveform
 
     # test full pipeline
-    encoded = cm.encode_modulate(data)
+    encoded = cm.encode_modulate(data_tx)
     gen_waveform(encoded, '{}_full'.format(timestamp))
     parsed = parse_waveform('{}_full'.format(timestamp))
-    decoded = cm.demodulate_decode(parsed)
-    assert np.array_equal(data, decoded)
+    data_rx = cm.demodulate_decode(parsed)
+
+    for i, bit in enumerate(data_tx):
+        assert bit == data_rx[i]
